e368dc0400695b9383542729013f597713f76e11
Khadija Noor <you@example.com>
2021-10-15 16:24:29 +0500

    Add form.

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './src/store/store'
+import { store, persistor } from './src/store/store';
 import ThemeProvider from './src/providers/themeProvider';
 import Wrapper from './src/views';
 
