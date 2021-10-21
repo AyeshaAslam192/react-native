437baa62fb839ffcb019b5e842cf79b5ab58ddf8
Khadija Noor <you@example.com>
2021-10-21 12:54:23 +0500

    Add theming to dropdown.

--- src/components/common/Smart/AppDatePicker/index.js
+++ src/components/common/Smart/AppDatePicker/index.js
@@ -1,32 +1,74 @@
 import React, { useState } from 'react';
 import { Button, View } from "react-native";
-import DateTimePickerModal from "react-native-modal-datetime-picker";
+// import DateTimePickerModal from "react-native-modal-datetime-picker";
+import DateTimePicker from '@react-native-community/datetimepicker';
+import styles from "./AppDatePicker.styles";
 
 const AppDatePicker = () => {
-  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  // const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
 
-  const showDatePicker = () => {
-    setDatePickerVisibility(true);
+  // const showDatePicker = () => {
+  //   setDatePickerVisibility(true);
+  // };
+
+  // const hideDatePicker = () => {
+  //   setDatePickerVisibility(false);
+  // };
+
+  // const handleConfirm = (date) => {
+  //   hideDatePicker();
+  // };
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
+    setDate(currentDate);
   };
 
-  const hideDatePicker = () => {
-    setDatePickerVisibility(false);
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
   };
 
-  const handleConfirm = (date) => {
-    hideDatePicker();
+  const showDatepicker = () => {
+    showMode('date');
   };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
+
 
   return (
-    <View>
-      <Button title="Show Date Picker" onPress={showDatePicker} />
+    <View style={styles.datePickerStyle}>
+      {/* <Button title="Show Date Pickerrr" onPress={showDatePicker} />
       <DateTimePickerModal
         isVisible={isDatePickerVisible}
         mode="date"
         onConfirm={handleConfirm}
         onCancel={hideDatePicker}
         date={new Date()}
-      />
+      /> */}
+      <View>
+        <Button onPress={showDatepicker} title="Show date picker!" />
+      </View>
+      <View>
+        <Button onPress={showTimepicker} title="Show time picker!" />
+      </View>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={mode}
+          is24Hour={true}
+          display="default"
+          style={{backgroundColor: "red"}}
+          onChange={onChange}
+        />
+      )}
     </View>
   );
 };
--- src/components/common/Smart/AppDropdown/index.js
+++ src/components/common/Smart/AppDropdown/index.js
@@ -1,26 +1,33 @@
 import React, {useState} from 'react';
 import DropDownPicker from 'react-native-dropdown-picker';
 import styles from "./AppDropdown.styles";
+import { useTheme } from "../../../../providers/themeProvider";
 
 const AppDropdown = () => {
-    const [open, setOpen] = useState(false);
-    const [value, setValue] = useState(null);
-    const [items, setItems] = useState([
-      {label: 'Apple', value: 'apple'},
-      {label: 'Banana', value: 'banana'}
-    ]);
+  const { colors } = useTheme();
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    {label: 'Apple', value: 'apple'},
+    {label: 'Banana', value: 'banana'}
+  ]);
   
-    return (
-      <DropDownPicker
-        open={open}
-        value={value}
-        items={items}
-        setOpen={setOpen}
-        setValue={setValue}
-        setItems={setItems}
-        containerStyle={ styles.dropdownStyle }
-      />
-    );
+  return (
+    <DropDownPicker
+      open={open}
+      value={value}
+      items={items}
+      showArrowIcon={false}
+      setOpen={setOpen}
+      setValue={setValue}
+      setItems={setItems}
+      style={{backgroundColor: colors.cardBg, borderColor: colors.cardTxt}}
+      containerStyle={ [styles.dropdownStyle, { backgroundColor: colors.cardBg }] }
+      dropDownContainerStyle={{backgroundColor: colors.cardBg, borderColor: colors.cardTxt}}
+      textStyle = {{ color: colors.cardTxt }}
+      modalProps={{ animationType: "fade" }}
+    />
+  );
 }
 
 export default AppDropdown;
